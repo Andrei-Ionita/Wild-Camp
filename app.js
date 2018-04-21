a2b057ecb7c01e9dd2b1d7d228ad1a12e276e919
Andrei-Ionita <32931225+Andrei-Ionita@users.noreply.github.com>
2018-04-21 10:29:46 +0300

    connect to db

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var contactRoutes = require("./routes/contact");
 
 var app = express();
 var urlDB = process.env.MONGODB_MLAB;
-MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+MongoClient.connect(MONGODB_URI, function(err, db) {
     if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
         } else {
